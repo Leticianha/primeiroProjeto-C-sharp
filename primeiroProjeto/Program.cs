4acc27c0b932426be8ea9de8b8d0fbc2ec8ace37
Leticia Cristina <letici4cr1s@gmail.com>
2024-04-17 16:37:15 -0300

    C#

---
+++ primeiroProjeto/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Olá Mundo");
+
+            int idade = 23;
+            Console.WriteLine("A minha idade é: " + idade);
+
+
+            *//*OPERAÇÕES MATEMÁTICAS*//*
+            int numero1 = 9;
+            int numero2 = 3;
+
+            int soma = numero1 + numero2;
+            int subtracao = numero1 - numero2;
+            int multiplicacao = numero1 * numero2;
+            int divisao = numero1 / numero2;
+
+            Console.WriteLine(divisao);
+            Console.WriteLine(multiplicacao);
+            Console.WriteLine(subtracao);
+            Console.WriteLine(soma);
+
+            double salario = 1500.86;
+            Console.WriteLine(salario);*/
+
+            /*ATIVIDADE 1*/
+
+            /*1 - Joaquim trabalha em uma empresa e recebe R$35.00 reais por hora
+            e trabalha 8 horas por dia.Ele solicitou um programa que calcule
+            quanto ao final de um dia ele deve receber*/
+
+            /*int salario = 35;
+            int horas = 8;
+            int dia = salario * horas;
+            Console.WriteLine("ATIVIDADE 1");
+            Console.WriteLine("No final do dia você irá receber: R$" + dia + " reais");*/
+
+            /*ATIVIDADE 2*/
+
+            /*2 - Cada caixa de bombom vem 27 unidades, Pamela comprou 16
+            unidades da caixa de bombom, quantos bombons ela tem?*/
+
+            /*int unidades = 27;
+            int compra = 16;
+            int quant = unidades * compra;
+            Console.WriteLine("ATIVIDADE 2");
+            Console.WriteLine("Pamola tem " + quant + " caixas de bombons.");*/
+
+            /*ATIVIDADE 3*/
+
+            /*3 - Faça um programa para ler dois valores inteiros, e depois mostrar
+            na tela a soma desses números.*/
+
+            /*int num1 = 62;
+            int num2 = 33;
+            int soma = num1 + num2;
+            Console.WriteLine("ATIVIDADE 3");
+            Console.WriteLine("A soma dos números é: " + soma);*/
+
+            /*ATIVIDADE 4*/
+
+            /*4 - Uma loja vendeu 300 garrafas de água, cada caixa vem 20 garrafas,
+            quantas caixas eles usaram?*/
+
+            /*int venda = 300;
+            int caixa = 20;
+            int divisao = venda / caixa;
+            Console.WriteLine("ATIVIDADE 4");
+            Console.WriteLine("Eles usaram " + divisao + " caixas.");*/
+
+            /*ATIVIDADE 5*/
+
+            /*5 - A empresa TERMAL conhecida pelos seus serviços de instalação de
+            banheiras solicitou um sistema que para somar os serviços feitos pelos
+            seus funcionários. Os serviços são:
+
+                Instalação – R$300.00
+                Limpeza – R$150.00
+                Manutenção – 499.99
+
+            Além dos serviços, em uma promoção de novos clientes, a empresa
+            está dando um desconto de R$ 50.00.Quando o cliente deverá pagar
+            para a empresa ?*/
+
+            /*int instalacao = 300;
+            int limpeza = 150;
+            double manutencao = 499.99;
+            int desconto = 50;
+            double sistema = (instalacao + limpeza + manutencao) - desconto;
+            Console.WriteLine("ATIVIDADE 5");
+            Console.WriteLine("O cliente deverá pagar para a empresa: R$" + sistema + " reais.");*/
+        }
+    }
+}
